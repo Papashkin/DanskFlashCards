91ab97ac93dc6dc4277e3f556f7ccc04727599b0
Pavel Antoshkin <34233903+Papashkin@users.noreply.github.com>
2023-10-18 21:07:34 +0200

    feature: Splash screen implementation (#3)

--- app/src/main/java/com/antsfamily/danskflashcards/MainActivity.kt
+++ app/src/main/java/com/antsfamily/danskflashcards/MainActivity.kt
@@ -3,6 +3,7 @@ package com.antsfamily.danskflashcards
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.antsfamily.danskflashcards.navigation.Navigator
 import com.antsfamily.danskflashcards.ui.theme.DanskFlashCardsTheme
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DanskFlashCardsTheme {
                 Navigator()
