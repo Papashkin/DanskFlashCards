e173e9d0157338c4a15823964bdbe4ef53909b08
pavelantoshkin <paavoantoshkin@gmail.com>
2025-03-20 16:04:03 +0100

    task: update release workflow for CI/CD

--- app/src/main/java/com/antsfamily/danskflashcards/ui/about/AboutScreen.kt
+++ app/src/main/java/com/antsfamily/danskflashcards/ui/about/AboutScreen.kt
@@ -1,6 +1,5 @@
 package com.antsfamily.danskflashcards.ui.about
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -18,7 +16,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
