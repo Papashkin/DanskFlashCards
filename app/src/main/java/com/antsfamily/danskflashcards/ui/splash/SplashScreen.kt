34d327cae1172a084b35658a4e7c664c7981776f
pavelantoshkin <paavoantoshkin@gmail.com>
2025-04-10 09:00:13 +0200

    task: add Firebase Crashlytics

--- app/src/main/java/com/antsfamily/danskflashcards/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/antsfamily/danskflashcards/ui/splash/SplashScreen.kt
@@ -5,15 +5,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
