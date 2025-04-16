bf2d1703f28c095cbbcb5bb385ce2ca5060b8ac6
pavelantoshkin <paavoantoshkin@gmail.com>
2025-04-16 09:42:41 +0200

    task: bump version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = versionCode()
-        versionName = "1.1.0"
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
