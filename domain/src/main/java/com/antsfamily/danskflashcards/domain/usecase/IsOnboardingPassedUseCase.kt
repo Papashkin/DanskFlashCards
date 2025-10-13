559e70eac90b3f7785579bf9073999c15d51234f
Pavel Antoshkin <34233903+Papashkin@users.noreply.github.com>
2025-10-13 21:29:59 +0200

    fix: ANR issue from Firebase (#98)

--- domain/src/main/java/com/antsfamily/danskflashcards/domain/usecase/IsOnboardingPassedUseCase.kt
+++ domain/src/main/java/com/antsfamily/danskflashcards/domain/usecase/IsOnboardingPassedUseCase.kt
@@ -1,13 +1,15 @@
 package com.antsfamily.danskflashcards.domain.usecase
 
 import com.antsfamily.danskflashcards.domain.repository.DataRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class IsOnboardingPassedUseCase @Inject constructor(
     private val repository: DataRepository
 ) {
 
-    suspend operator fun invoke(): Boolean {
-        return repository.isOnboardingPassed()
+    suspend operator fun invoke(): Boolean = withContext(Dispatchers.IO) {
+        repository.isOnboardingPassed()
     }
 }
