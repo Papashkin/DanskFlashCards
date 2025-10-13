f444d81d66140f97e4da8dee7160238eb5c3cb24
pavelantoshkin <paavoantoshkin@gmail.com>
2025-10-13 21:23:26 +0200

    fix: ANR issue from Firebase

--- domain/src/main/java/com/antsfamily/danskflashcards/domain/usecase/SignInUseCase.kt
+++ domain/src/main/java/com/antsfamily/danskflashcards/domain/usecase/SignInUseCase.kt
@@ -4,30 +4,28 @@ import com.antsfamily.danskflashcards.domain.model.SignInType
 import com.antsfamily.danskflashcards.domain.model.UserDomain
 import com.antsfamily.danskflashcards.domain.repository.AuthRepository
 import com.antsfamily.danskflashcards.domain.repository.DataRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SignInUseCase @Inject constructor(
     private val dataRepository: DataRepository,
     private val authRepository: AuthRepository
 ) {
-    suspend operator fun invoke(type: SignInType): UserDomain? {
-        try {
-            val result = when (type) {
-                SignInType.GOOGLE -> signInWithGoogle()
-                SignInType.FACEBOOK -> null
-            }
-
-            if (result?.id != null) {
-                val allUsersIDs = dataRepository.getUsers().map { it.id }
-                if (result.id !in allUsersIDs)  {
-                    dataRepository.updateUserName(result.id, result.username)
-                }
-            }
-
-            return result
-        } catch (e: Exception) {
-            throw e
+    suspend operator fun invoke(type: SignInType): UserDomain? = withContext(Dispatchers.IO) {
+        val result = when (type) {
+            SignInType.GOOGLE -> signInWithGoogle()
+            SignInType.FACEBOOK -> null
         }
+
+        if (result?.id != null) {
+            val allUsersIDs = dataRepository.getUsers().map { it.id }
+            if (result.id !in allUsersIDs) {
+                dataRepository.updateUserName(result.id, result.username)
+            }
+        }
+
+        result
     }
 
     private suspend fun signInWithGoogle(): UserDomain? {
