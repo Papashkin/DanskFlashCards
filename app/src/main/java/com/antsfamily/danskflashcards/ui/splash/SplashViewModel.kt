340326f7d1f012a8061d97a68a062f17b6d0de58
Pavel Antoshkin <34233903+Papashkin@users.noreply.github.com>
2025-10-17 11:38:50 +0200

    task: improve update check on Splash screen (#101)

--- app/src/main/java/com/antsfamily/danskflashcards/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/antsfamily/danskflashcards/ui/splash/SplashViewModel.kt
@@ -4,18 +4,21 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.antsfamily.danskflashcards.core.model.CurrentUserItem
+import com.antsfamily.danskflashcards.core.model.mapToErrorType
 import com.antsfamily.danskflashcards.domain.usecase.GetLoggedInUserUseCase
 import com.antsfamily.danskflashcards.domain.usecase.IsOnboardingPassedUseCase
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.install.model.AppUpdateType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withTimeout
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,6 +27,10 @@ class SplashViewModel @Inject constructor(
     private val getLoggedInUserUseCase: GetLoggedInUserUseCase,
     private val isOnboardingPassedUseCase: IsOnboardingPassedUseCase,
 ) : ViewModel() {
+
+    companion object {
+        private const val UPDATES_TIMEOUT = 2000L
+    }
 
     private val _navigationToHomeFlow = MutableSharedFlow<CurrentUserItem>()
     val navigationToHomeFlow: SharedFlow<CurrentUserItem> = _navigationToHomeFlow.asSharedFlow()
@@ -40,21 +47,26 @@ class SplashViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            delay(500)
             checkForUpdates()
         }
     }
 
     private suspend fun checkForUpdates() {
         try {
-            val updateInfo = appUpdateManager.appUpdateInfo.await()
+            val updateInfo = withTimeout(UPDATES_TIMEOUT) {
+                appUpdateManager.appUpdateInfo.await()
+            }
             if (updateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
                 _startAppUpdateFlow.emit(updateInfo)
             } else {
                 checkIsUserLoggedIn()
             }
+        } catch (e: TimeoutCancellationException) {
+            logError(e)
+            checkIsUserLoggedIn()
         } catch (e: Exception) {
             logError(e)
+            checkIsUserLoggedIn()
         }
     }
 
